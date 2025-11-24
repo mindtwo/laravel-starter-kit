d14ca6d86f45041a5170c600012947bc469cf238
Andreas Schneider <schneider@mindtwo.de>
2025-11-24 18:30:52 +0100

    refactor: apply suggested changes

--- resources/js/lib/index.ts
+++ resources/js/lib/index.ts
@@ -11,6 +11,8 @@ if (typeof window !== 'undefined') {
 
     if (token) {
         axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+    } else {
+        axios.defaults.withCredentials = true;
     }
 }
 
